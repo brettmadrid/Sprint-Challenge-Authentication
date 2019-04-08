b8cbad3549e4845338b998fc5d59b5ef8db0ad09
brettmadrid <brettmadrid9@gmail.com>
2019-04-07 20:16:21 -0700

    added logout component

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,69 +1,15 @@
-import React from "react";
-import axios from "axios";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import React from 'react';
 
-class Login extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
+export default class Logout extends React.Component {
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const endpoint = "http://localhost:9000/api/login";
-
-    axios
-      .post(endpoint, this.state)
-      .then(res => {
-        localStorage.setItem("jwt", res.data.token);
-        this.props.history.push("/jokes");
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  };
+  componentDidMount() {
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login');
+  }
 
   render() {
     return (
-      <>
-        <h4>Sign In</h4>
-        <Form onSubmit={this.handleSubmit}>
-          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="username" className="mr-sm-2">
-              Username
-            </Label>
-            <Input
-              name="username"
-              id="username"
-              placeholder="username"
-              value={this.state.username}
-              onChange={this.handleInputChange}
-              type="text"
-            />
-          </FormGroup>
-          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="password" className="mr-sm-2">
-              Password
-            </Label>
-            <Input
-              name="password"
-              id="password"
-              placeholder="password"
-              value={this.state.password}
-              onChange={this.handleInputChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button type="submit">Submit</Button>
-        </Form>
-      </>
-    );
+      <h2>Logout</h2>
+    )
   }
 }
-
-export default Login;
